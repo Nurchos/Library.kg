9f72a8de5f14f1561fd7d439574a00d3e203ec02
Nurchos <kimleesayakat10.com>
2025-02-08 15:16:40 +0600

    сделал модели с выводом информации на вебсайте

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.book_list_view, name='book_list'),
+    path('book_detail/<int:id>/', views.book_detail_view, name='film_detail'),
+
     path('about_me/', views.about_me, name='about_me'),
     path('photo/', views.photo, name='photo'),
     path('time/', views.time, name='time')
